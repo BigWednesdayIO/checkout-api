8a8a26755e00d654e034fbf7dd3ae3ee3ba7229d
Chris Impey <chris@bigwednesday.io>
2015-11-06 09:53:08 +0000

    return a checkout resource response, containing the line items

--- spec/checkout.js
+++ spec/checkout.js
@@ -5,33 +5,57 @@ const _ = require('lodash');
 const expect = require('chai').expect;
 const server = require('../lib/server');
 
-describe('Checkout', () => {
-  it('returns a 200 response on success', done => {
+const basket = {
+  line_items: [
+    {product_id: 'a', quantity: 1},
+    {product_id: 'b', quantity: 4},
+    {product_id: 'c', quantity: 1}
+  ]
+};
+
+const performCheckout = () => {
+  return new Promise((resolve, reject) => {
     server((err, server) => {
       if (err) {
-        return done(err);
+        return reject(err);
       }
 
-      server.inject({url: '/checkouts', method: 'POST'}, response => {
-        expect(response.statusCode).to.equal(200);
-        done();
+      server.inject({url: '/checkouts', method: 'POST', payload: {basket}}, response => {
+        return resolve(response);
       });
     });
   });
+};
 
-  it('returns a url for the created order', done => {
-    server((err, server) => {
-      if (err) {
-        return done(err);
-      }
+describe('Checkout', () => {
+  it('returns a 201 response on success', () => {
+    return performCheckout()
+      .then(response => {
+        expect(response.statusCode).to.equal(201);
+      });
+  });
 
-      server.inject({url: '/checkouts', method: 'POST'}, response => {
+  it('returns the checkout location header', () => {
+    return performCheckout()
+      .then(response => {
+        expect(response.headers.location).to.equal(`/checkouts/${response.result.id}`);
+      });
+  });
+
+  it('returns a url for the created order', () => {
+    return performCheckout()
+      .then(response => {
         const orderLink = _.find(response.result.links, {rel: 'order'});
 
         expect(orderLink).to.exist;
         expect(orderLink.href).to.match(/^\/orders\/.*/);
-        done();
       });
-    });
+  });
+
+  it('returns the checked out line items', () => {
+    return performCheckout(basket)
+      .then(response => {
+        expect(response.result.line_items).to.deep.equal(basket.line_items);
+      });
   });
 });
