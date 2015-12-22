e5245f88b433efc796beac1807dfdb37577cd538
toboid <toboid@users.noreply.github.com>
2015-12-22 11:13:16 +0000

    Get orders for customer

--- spec/orders.js
+++ spec/orders.js
@@ -12,7 +12,7 @@ const adminToken = signToken({scope: ['admin']});
 describe('/orders', function () {
   this.timeout(10000);
 
-  const checkouts = [];
+  let checkouts;
   const checkoutCustomerASupplierA = new CheckoutBuilder()
                                       .withCustomerId('customer-a')
                                       .withOrderForms([new OrderFormBuilder().withSupplierId('supplier-a').build()])
@@ -26,7 +26,8 @@ describe('/orders', function () {
                                       .withOrderForms([new OrderFormBuilder().withSupplierId('supplier-a').build()])
                                       .build();
 
-  before(() => {
+  beforeEach(() => {
+    checkouts = [];
     return specRequest({
       url: '/checkouts',
       method: 'POST',
@@ -76,6 +77,42 @@ describe('/orders', function () {
       it('returns http 403 without admin scope', () => {
         return specRequest({
           url: '/orders',
+          method: 'GET',
+          headers: {authorization: signToken({scope: ['customer:customer-a']})}
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(403);
+        });
+      });
+    });
+
+    describe('customer scope', () => {
+      it('returns all order resources for customer', () => {
+        return specRequest({
+          url: '/orders?customer_id=customer-a',
+          method: 'GET',
+          headers: {authorization: signToken({scope: ['customer:customer-a']})}
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(200);
+          expect(response.result).to.eql(_.filter(checkouts, {customer_id: 'customer-a'}).map(_.partialRight(_.omit, 'links')));
+        });
+      });
+
+      it('returns 403 without customer_id query', () => {
+        return specRequest({
+          url: '/orders',
+          method: 'GET',
+          headers: {authorization: signToken({scope: ['customer:customer-a']})}
+        })
+        .then(response => {
+          expect(response.statusCode).to.equal(403);
+        });
+      });
+
+      it('returns 403 requesting customer orders without correct scope', () => {
+        return specRequest({
+          url: '/orders?customer_id=customer-b',
           method: 'GET',
           headers: {authorization: signToken({scope: ['customer:customer-a']})}
         })
